55c17fcca1ba53d63e637b468a56f795c46b16e8
Vitor Hugo de Souza <vitorhugodesouza46@gmail.com>
2024-11-21 21:50:33 -0300

    uptade

--- js/games.js
+++ js/games.js
@@ -1,5 +1,6 @@
 const spanPlayer = document.querySelector(".player");
 const timer = document.querySelector(".timer");
+const grid = document.querySelector(".grid")
 
 let currentTime = 0;
 
@@ -8,11 +9,47 @@ let currentTime = 0;
 window.onload = () => {
     
     spanPlayer.innerHTML = localStorage.getItem("player");
-
+    startTimer();
 };
 
 // função para iniciar o tempo 
 
+const startTimer = () => {
+
+    this.loop = setInterval(() => {
+        
+        currentTime++;
+
+        timer.innerHTML = currentTime;
+
+    }, 1000);
+
+};
+
+// array dos personagens das cartas 
+
+const characters = [
+    "carta1.png",
+    "carta2.png",
+    "carta3.png",
+    "carta4.png",
+    "carta5.png",
+    "carta6.png",
+    "carta7.png",
+    "carta8.png",
+    "carta9.png",
+    "carta10.png"
+];
+
+// criar as cartas 
+
+const createCard = () => {
+
+    const card = document.createElement("div");
+    const front = document.createElement("div");
+    const back = document.createElement("div");
+    
+};
 
 
 
