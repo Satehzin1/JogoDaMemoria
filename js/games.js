1dd3b174f2f5656ba556c213b20a5a8276093355
Vitor Hugo de Souza <vitorhugodesouza46@gmail.com>
2024-11-21 21:26:52 -0300

    uptade

--- js/games.js
+++ js/games.js
@@ -1,3 +1,17 @@
+const spanPlayer = document.querySelector(".player");
+const timer = document.querySelector(".timer");
+
+let currentTime = 0;
+
+// Quando a Janela carregar
+
+window.onload = () => {
+    
+    spanPlayer.innerHTML = localStorage.getItem("player");
+
+};
+
+// função para iniciar o tempo 
 
 
 
